7acc4aa9f16f48ebee8e2b0abe5f1285cc1da3c0
storozh2brah <ldsnik@gmail.com>
2025-01-10 11:03:21 +0700

    task_2

--- src/com/ibs/basic/Main.java
+++ src/com/ibs/basic/Main.java
@@ -3,5 +3,11 @@ package com.ibs.basic;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+//      1. Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значение
+        double maxFloatValue = 3.14;
+//      2. Создайте переменную целочисленного типа с наименьшим диапазоном чисел. Присвойте ей наибольшее возможное значение
+        byte minIntValue = Byte.MAX_VALUE;
     }
 }

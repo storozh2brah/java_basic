9ef8365865f75f8947d0304a571d4642e9930bd1
storozh2brah <ldsnik@gmail.com>
2025-01-19 17:53:48 +0700

    task_10

--- src/com/ibs/basic/Main.java
+++ src/com/ibs/basic/Main.java
@@ -204,5 +204,33 @@ public class Main {
         for (int i = 0; i < arraySize2; i++) {
             System.out.println("Элемент №" + (i + 1) + " * " + average2 + " = " + array2[i] * average2);
         }
+//      task_9
+//      Напишите программу, где пользователь вводит данные с клавиатуры в матрицу,
+//      а после этого произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3.
+//      Размерность матрицы задается пользователем.
+        int matrixSize1, matrixSize2;
+        System.out.println("Введите размер матрицы - строки: ");
+        while (true) {
+            matrixSize1 = sc.nextInt();
+            if (matrixSize1 != 0) break;
+        }
+        System.out.println("Введите размер матрицы - столбцы: ");
+        while (true) {
+            matrixSize2 = sc.nextInt();
+            if (matrixSize2 != 0) break;
+        }
+        double[][] matrixArray = new double[matrixSize1][matrixSize2];
+        System.out.println("Заполните матрицу числами: ");
+        for (int i = 0; i < matrixSize1; i++) {
+            for (int j = 0; j < matrixSize2; j++) {
+                matrixArray[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("Первая строка матрицы * 3: ");
+        for (int i = 0; i < matrixSize1; i++) {
+            for (int j = 0; j < matrixSize2; j++) {
+                System.out.println("Элемент № " + (i + 1) + ": " + matrixArray[i][j] + " * 3 = " + matrixArray[i][j] * 3);
+            }
+        }
     }
 }

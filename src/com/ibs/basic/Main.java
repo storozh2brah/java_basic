fc91756b295ff83fb47dcfde0cf2c7e705de5e4a
storozh2brah <ldsnik@gmail.com>
2025-01-19 17:05:02 +0700

    task_7

--- src/com/ibs/basic/Main.java
+++ src/com/ibs/basic/Main.java
@@ -148,5 +148,25 @@ public class Main {
                 }
                 break;
         }
+//      task_7
+//      1. Пользователь вводит размер массива и данные с клавиатуры в массив
+        int arraySize;
+        System.out.println("Введите размер массива: ");
+        while (true) {
+            arraySize = sc.nextInt();
+            if (arraySize != 0) break;
+        }
+        double[] array = new double[arraySize];
+        System.out.println("Заполните массив числами: ");
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = sc.nextDouble();
+        }
+//      2. Сравнить элементы массива с заранее заданными константами x, y, z.
+//      3. Если массив содержит хотя бы одну из констант, вывести текст "Данное значение имеется в константах".
+        for (int i = 0; i < arraySize; i++) {
+            if (array[i] == x || array[i] == y || array[i] == z) {
+                System.out.println("Данное значение имеется в константах");
+            }
+        }
     }
 }
